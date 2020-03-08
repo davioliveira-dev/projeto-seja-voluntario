3a07c8c6edb8efee20f973dc3189a1838c8aef3d
unknown <davioliveira.java@gmail.com>
2020-03-08 09:41:44 -0300

    Adicionando pasta com assets!

---
+++ assets/js/forms.js
@@ -0,0 +1,18 @@
+const validateField = (field,message) => {
+    if (!field.value && field.value.trim() == "") {
+        alert(message);
+        return false
+    }
+    return true;
+}
+
+const validateNotice = form => {
+    return validateField(form.email, "Email deve ser informado!") 
+    && validateField(form.descricao, "Descrição deve ser informada!")
+     && validateField(form.titulo, "O título deve ser informado!")
+      && validateField(form.cidade, "A cidade deve ser informada!")
+}
+
+const validateSearch = form => {
+    return validateField(form.key, "Informe o que deseja buscar!");
+}
